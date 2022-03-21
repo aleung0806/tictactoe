841d6fbf338446bb7285efa0157374eb15eb43ec
aleung0806 <aleung0806@gmail.com>
2022-03-20 21:32:45 -0700

    everything should be working

--- script.js
+++ script.js
@@ -1,53 +1,86 @@
+const playerFactory = (name, marker) => {
+    console.log(`${name}`);
+    document.querySelector(`#${marker}_name`).innerHTML = name;
+
+    let score = 0;
+    const win = () => {
+        score++;
+        document.querySelector(`#${marker}_score`).innerHTML = `${score} points`;
+
+    }
+    return {name, marker, win};
+};
+
+
 const gameBoard = (()=>{
     let board = [['','',''],['','',''],['','','']];
     let plays = 0;
     let turn = 'X';
+    let player1;
+    let playe2;
 
     const clear = () => {
         board = [['','',''],['','',''],['','','']];
         plays = 0;
         turn = 'X';
+        document.querySelectorAll('.tile').forEach((tile) => {
+            tile.src = `images/blank.jpg`;
+        });
+        document.querySelector('#replay_button').style.display = "none";
+        document.querySelector('#display').innerHTML = `${player1.name}'s turn`;
     };
+
     const init = () => {
+        document.querySelector('#play_button').addEventListener('click', () => {
+            document.querySelector('#body').style.display = "block";
+            document.querySelector('#name_div').style.display = "none";
+            name1 = document.querySelector('#name1').value;
+            name2 = document.querySelector('#name2').value;
+
+            player1 = playerFactory(name1, "X");
+            player2 = playerFactory(name2, "O");
+
+        });
         document.querySelectorAll('.tile').forEach((tile) => {
             tile.addEventListener('click', () => {
-                let marker = tile.dataset.marker;
-                if (marker === 'blank'){
-                    let xy = tile.id.split('');
+                let xy = tile.id.split('');
+                if (board[xy[0]][xy[1]] === ''){
                     tile.src = `images/${turn}.jpg`;
-                    tile.dataset.marker = turn;
-                    play(xy[0], xy[1], turn);
+                    board[xy[0]][xy[1]] = turn;
+                    plays++;
                     flipTurn();
                     if (check()){
-                        document.querySelector('#turn').innerHTML = check();
+                        document.querySelector('#replay_button').style.display = "block";
                     }
                 }
             })
         });
-    };
-    const play = (x, y, marker) => {
-        board[x][y] = marker; 
-        plays++;
+        document.querySelector('#replay_button').addEventListener('click', () => {
+            clear();
+        });
     };
     const flipTurn = () => {
         if (turn === 'X'){
-            document.querySelector('#turn').innerHTML = "O's turn";
+            document.querySelector('#display').innerHTML = `${player2.name}'s turn`;
             turn = 'O';
         }else{
-            document.querySelector('#turn').innerHTML = "X's turn";
+            document.querySelector('#display').innerHTML = `${player1.name}'s turn`;
             turn = 'X';
         }
-
     };
     const check = () => {
-        console.log(board);
         if (checkX()){
-                return "X wins";
+            document.querySelector('#display').innerHTML = `${player1.name} wins`;
+            player1.win();
         } else if (checkO()){
-                return "O wins";
+            document.querySelector('#display').innerHTML =  `${player2.name} wins`;
+            player2.win();
         } else if (plays === 9){
-            return "Tie";
+            document.querySelector('#display').innerHTML = "Tie";
+        }else{
+            return false;
         }
+        return true;
     };
     const checkX = () => {
         return ((board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X') ||
@@ -71,14 +104,13 @@ const gameBoard = (()=>{
         (board[0][2] === 'O' && board[1][1] === 'O' && board[2][0] === 'O'));
 
     };
-    return {board, plays, turn, clear, init, play, check, checkX, checkO};
+    return {name1, name2, board, plays, turn, clear, init, check, checkX, checkO};
 })();
 
-const playerFactory = (name, marker) => {
-    return {name, marker};
-}
+
 
 gameBoard.init();
+
 
 
 
